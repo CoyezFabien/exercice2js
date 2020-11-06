60bfbc95708271d4093b115c7d8d8558d95c3ef7
CoyezFabien <coyez.fabien@gmail.com>
2020-11-06 17:51:33 +0100

    exercice2js

---
+++ Nouveau dossier/JS/ex2.js
@@ -0,0 +1,11 @@
+var lastName = 'Coyez';
+// on met des guillement simple quand c'est une valeur textuelle si la valeur est un chiffre on ne mettra pas de guillement met ex var LastName = 16; attention toujours fermer avec un points virgule
+var firstName = 'Fabien';
+var city = 'Morcenx';
+// j'ai marqué 3 vars en leur attribuant un nom et leur affectant chacune une valeur
+alert('Nom :' + lastName + '\n' + 'Prenom :' + firstName + '\n' + 'Ville :' + city);
+// j'ai mis une alerte car c'est l'exercice qui est demandé dedans 
+
+// en premier une croche avec Nom car il doit apparaitre avant Coyez qui est marque automatiquement par la var qui est relié avec lui ensuite viens un '\' pour faire un saut de ligne on fait la meme chose pour le reste le symbole + entre les mots permet d'attaché des chaines ensemble,
+
+// une chaine  type String ou chaîne de caractères. Une chaine de caractères est une séquence de caractères, ou ce qu’on appelle communément un texte.toute valeur stockée dans une variable en utilisant des guillemets ou des apostrophes sera considérée comme une chaine de caractères, et ceci même dans le cas où nos caractères sont à priori des chiffres comme "29" par exemple.
